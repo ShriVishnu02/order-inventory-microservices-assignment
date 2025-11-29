33c698fe5717ce01dc0645c501af6dc01f3458d9
ShriVishnu02 <shrivishnu222@gmail.com>
2025-11-29 08:55:46 +0530

    feat: add repository layer classes

---
+++ inventory-service/src/main/java/com/assignment/microservices/inventory/repository/BatchRepository.java
@@ -0,0 +1,11 @@
+package com.assignment.microservices.inventory.repository;
+
+import com.assignment.microservices.inventory.model.Batch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface BatchRepository extends JpaRepository<Batch, Long> {
+    List<Batch> findByProductIdOrderByExpiryDateAsc(Long productId);
+}
